903e8a9dc17f953f8a88a42f6a4e977d0cfee6c4
baredmark583 <baredmark583@gmail.com>
2025-09-10 11:37:24 +0300

    Initial setup for Render

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+
+// FIX: `__dirname` is not available in ES modules. This defines a compatible equivalent.
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
     return {
